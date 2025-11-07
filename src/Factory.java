bd083107842307373110f1d8995a6d7f2c5c9dbd
michaelrosa <michael_rosa28@milton.edu>
2025-11-07 08:38:47 -0500

    added constructor to factory

--- src/Factory.java
+++ src/Factory.java
@@ -1,7 +1,38 @@
 public class Factory {
+//scope is whole class
+public int yearfounded = 1798;
+public String factoryName = "car factory";
+public boolean isOpen = true;
+
 
     public static void main(String[] args) {
-        System.out.println("welcome to Michael's Factory");
+        System.out.println("welcome to Michael's factory");
+        new Factory();
+
     }
+
+
+    //constructor is a very special type of method which has the exact same name as the class
+    public Factory(){
+        isOpen = false;
+        //scope for manager is only in the contructory
+        String manager = "Santa";
+        factoryinfo();
+
+        //todo: chane the variables yearfounded to be a different value
+        //call factoryInfo();
+
+    }
+    //make a method called factoryinfo
+
+    public void factoryinfo(){
+
+        System.out.println(yearfounded);
+        System.out.println(factoryName);
+        System.out.println(isOpen);
+        System.out.println();
+
+    }
+
 
 }
