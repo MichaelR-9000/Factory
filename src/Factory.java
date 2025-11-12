c8c205739b7c85074e5f9de65dd11dc695d41314
michaelrosa <michael_rosa28@milton.edu>
2025-11-12 09:04:01 -0500

    added airplane

--- src/Factory.java
+++ src/Factory.java
@@ -1,8 +1,8 @@
 public class Factory {
 //scope is whole class
-public int yearfounded = 1798;
-public String factoryName = "car factory";
-public boolean isOpen = true;
+public int yearfounded = 3005;
+public String factoryName = "airplane factory";
+public boolean isOpen = false;
 
 
     public static void main(String[] args) {
@@ -12,7 +12,9 @@ public boolean isOpen = true;
     }
 
 
-    //constructor is a very special type of method which has the exact same name as the class
+    ///1. constructor is a very special type of method which has the exact same name as the class
+    /// 2. is a method
+    ///3. no void
     public Factory(){
         isOpen = false;
         //scope for manager is only in the contructory
@@ -21,6 +23,25 @@ public boolean isOpen = true;
 
         //todo: chane the variables yearfounded to be a different value
         //call factoryInfo();
+
+        //type varName = value
+        //object of type airplane
+        airplane plane1 = new airplane();
+        System.out.println(plane1.name);
+        plane1.capacity = 300;
+        plane1.isDoubleDecker = true;
+        plane1.length = 3;
+        plane1.model = "bigplane";
+        System.out.println(plane1.model);
+        airplane plane2 = new airplane();
+
+        System.out.println(plane2.capacity);
+
+        //HW: make 2 more airplane objects
+        //for one of the objects change all the instance variables
+        // for all or both objects print all the instance variables
+
+
 
     }
     //make a method called factoryinfo
---
+++ src/airplane.java
@@ -0,0 +1,22 @@
+public class airplane {
+    /// instance variables
+    /// 1. used in the whole class
+    /// 2. should be at the top of the class
+    /// 3. must have public in front
+    public int capacity;
+    public String name;
+    public String model;
+    public boolean isDoubleDecker;
+    public int length;
+    public airplane(){
+        capacity = 250;
+        name = "Jacob6";
+        model = "boeing 737";
+        isDoubleDecker = false;
+        length = 150;
+
+
+
+    }
+
+}
